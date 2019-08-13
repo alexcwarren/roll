13936318e3ff8adb761920f36d9ca8679bfa0d6d
Alex Warren <alexander.ch.warren@gmail.com>
2019-08-13 18:52:28 -0400

    Create roll.py

---
+++ roll.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''
+roll.py:
+Simulates rolls of different varieties of dice.
+'''
+
+__author__ = 'Alex Warren'
+
+if __name__ == '__main__':
+    print(__doc__)
+    print('Author: %s' % (__author__,))
