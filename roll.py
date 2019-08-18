da914041e5136446753f6477fea085f713388902
Alex Warren <alexander.ch.warren@gmail.com>
2019-08-17 22:59:19 -0400

    Change function name to validate_format; Change return object of validate_format to tuple; Implement print_outcome function

--- roll.py
+++ roll.py
@@ -9,21 +9,33 @@ __author__ = 'Alex Warren'
 
 import argparse
 import re
+import random
 
 
-def valid_format(dice):
+def validate_format(dice):
+    is_valid = False
+
     if re.match(r'\d{1,2}d\d{1,2}', dice):
-        die_type = re.findall(r'\d{1,2}$', dice)
+        num_dice = int( re.findall(r'^\d{1,2}', dice)[0] )
 
-        DIE_TYPES = [4, 6, 8, 10, 20]
+        die_type = int( re.findall(r'\d{1,2}$', dice)[0] )
 
+        faces = (4, 6, 8, 10, 20)
+        DIE_TYPES = frozenset(faces)
+        
         if die_type in DIE_TYPES:
-            return True
-    return False
+            is_valid = True
+    
+    return (is_valid, num_dice, die_type)
 
 
-def print_outcome(dice):
-    pass
+def print_outcome(num_dice, die_type):
+    sum = 0
+    for i in range(num_dice):
+        roll = random.randint(1, die_type)
+        sum += roll
+        print(roll, end=' ')
+    print('= %d' % sum)
 
 
 if __name__ == '__main__':
@@ -33,5 +45,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     for d in args.dice_list:
-        if valid_format(d):
-            print_outcome(d)
+        print('%s:' % d)
+        is_valid, num_dice, die_type = validate_format(d)
+        if is_valid:
+            print_outcome( num_dice, die_type )
+        else:
+            parser.print_help()
+            print('\n')
+            exit()
