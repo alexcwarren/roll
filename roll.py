a195959af9ee1e293f6dc7495a9899779d7f1b8d
Alex Warren <alexander.ch.warren@gmail.com>
2019-08-17 20:07:10 -0400

    Create and implement valid_format function

--- roll.py
+++ roll.py
@@ -8,10 +8,22 @@ Simulates rolls of different varieties of dice.
 __author__ = 'Alex Warren'
 
 import argparse
+import re
+
+
+def valid_format(dice):
+    if re.match(r'\d{1,2}d\d{1,2}', dice):
+        die_type = re.findall(r'\d{1,2}$', dice)
+
+        DIE_TYPES = [4, 6, 8, 10, 20]
+
+        if die_type in DIE_TYPES:
+            return True
+    return False
 
 
 def print_outcome(dice):
-    print(dice)
+    pass
 
 
 if __name__ == '__main__':
@@ -21,4 +33,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     for d in args.dice_list:
-        print_outcome(d)
+        if valid_format(d):
+            print_outcome(d)
